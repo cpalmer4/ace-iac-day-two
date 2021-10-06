9327389a377a46c42d382d3437f226660fdd8c4a
charles.palmer@shrgroupllc.com <charles.palmer@shrgroupllc.com>
2021-10-06 15:03:20 -0400

    formatting

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.0.2"
   backend "remote" {
-  organization = "Aviatrix-Test"
+    organization = "Aviatrix-Test"
     workspaces {
       name = "ace-iac-day-two"
     }
